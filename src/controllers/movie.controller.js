ab3b189299fd1ce3bde019295bf6dfb2f2dffcad
Lucky <sashankswain6@gmail.com>
2025-12-10 00:02:23 +0530

    Implement get movie functionality and update routes

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -53,7 +53,40 @@ const deleteMovie = async (req, res) => {
   }
 };
 
+const getMovies = async (req, res) => {
+  // To be implemented
+  try {
+    const { id } = req.params;
+    const movies = await Movie.findById(id);
+
+    if (!movies) {
+      return res.status(404).json({
+        message: "Movie not found",
+        data: null,
+        error: "No movie with the given ID",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Movie fetched successfully",
+      data: movies,
+      error: null,
+      success: true,
+    });
+  } catch (error) {
+    console.log("Error Occurred In GetMoviesController", error);
+    return res.status(500).json({
+      message: "Error fetching movies",
+      data: null,
+      error: error.message,
+      success: false,
+    });
+  }
+};
+
 module.exports = {
   createMovie,
   deleteMovie,
+  getMovies,
 };
--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -1,9 +1,10 @@
-const { createMovie, deleteMovie } = require("../controllers/movie.controller");
+const { createMovie, deleteMovie, getMovies } = require("../controllers/movie.controller");
 const validateMovie = require("../middlewares/movie.middlewares");
 
 const routes = (app) => {
   //this routes take express  app object as parameter
   app.post("/mba/api/v1/movies",validateMovie , createMovie);
+  app.get("/mba/api/v1/movies/:id", getMovies);
   app.delete("/mba/api/v1/movies/:id", deleteMovie);
 };
 
