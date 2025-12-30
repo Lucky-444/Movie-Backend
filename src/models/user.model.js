defec3ccd1435d92ac5d986b8657f4e4ee4f0143
Lucky <sashankswain6@gmail.com>
2025-12-31 02:43:30 +0530

    Implement user password encryption and validation middleware; add pre-save hook for password hashing and request validation for signup, signin, and password reset

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,5 +51,35 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+
+// This is called Mongoose Middleware (Hook) — specifically a:
+//Pre-save middleware / pre hook
+/**It runs automatically BEFORE the document is saved to MongoDB. 
+ *  intercept the save operation
+  *  hash the password
+  * then continue
+ */
+userSchema.pre("save", async function () {
+  // a trigger to encrypt the plain password before saving the user
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+
+});
+
+/**
+ * This is going to be an instance method for user, to compare a password
+ * with the stored encrypted password
+ * @param plainPassword -> input password given by user in sign in request
+ * @returns boolean denoting whether passwords are same or not ?
+ */
+
+//Instance Method (Schema Method)
+//Mongoose instance method
+userSchema.methods.isValidPassword = async function (plainPassword) {
+  const currentUser = this;
+  const compare = await bcrypt.compare(plainPassword, currentUser.password);
+  return compare;
+};
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
