62bb11a9bc2b5cd073d22da935479868bb4f3c84
Lucky <sashankswain6@gmail.com>
2026-01-05 02:58:55 +0530

    Cleaning the Update code

--- src/middlewares/user.middlewares.js
+++ src/middlewares/user.middlewares.js
@@ -1,15 +1,49 @@
 const { errorResponseBody } = require("../utils/responsebody");
+const User = require("../models/user.model");
+const { USER_ROLE } = require("../utils/constants");
 
-const validateUpdateUserRequest = (req, res, next) => {
-  // validate presence of atleast one of the two i.e. userRole or userStatus
-  if (!(req.body.userRole || req.body.userStatus)) {
-    errorResponseBody.err =
-      "Malformed request, please send atleast one parameter";
-    return res.status(400).json(errorResponseBody);
+const validateUpdateUserRequest = async (req, res, next) => {
+  try {
+    const loggedInUserId = req.user; // only id from token
+    const targetUserId = req.params.id;
+
+    // must send at least one
+    if (!(req.body.userRole || req.body.userStatus)) {
+      errorResponseBody.err =
+        "Malformed request, please send at least one parameter";
+      return res.status(400).json(errorResponseBody);
+    }
+
+    // get full user from DB
+    const loggedInUser = await User.findById(loggedInUserId);
+
+    // if not found
+    if (!loggedInUser) {
+      errorResponseBody.err = "User not found";
+      return res.status(404).json(errorResponseBody);
+    }
+
+    // user trying to update someone else
+    if (
+      loggedInUserId !== targetUserId &&
+      loggedInUser.userRole !== USER_ROLE.admin
+    ) {
+      errorResponseBody.err = "Not allowed to update other users";
+      return res.status(403).json(errorResponseBody);
+    }
+    // non-admin trying to change role/status
+    if (loggedInUser.userRole !== USER_ROLE.admin) {
+      errorResponseBody.err =
+        "You are not allowed to update role or status — admin only";
+      return res.status(403).json(errorResponseBody);
+    }
+
+    next();
+  } catch (err) {
+    console.log(err);
+    errorResponseBody.err = "Something went wrong";
+    return res.status(500).json(errorResponseBody);
   }
-  next();
 };
 
-module.exports = {
-  validateUpdateUserRequest,
-};
+module.exports = { validateUpdateUserRequest };
