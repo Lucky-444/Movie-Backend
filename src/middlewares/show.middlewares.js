904202fbc54feb5c023bdeccbcc79dd7fcf0d2cb
Lucky <sashankswain6@gmail.com>
2026-01-07 00:56:18 +0530

    cleaning the show middleware

--- src/middlewares/show.middlewares.js
+++ src/middlewares/show.middlewares.js
@@ -1,15 +1,44 @@
 const { STATUS } = require("../utils/constants");
+const { errorResponseBody } = require("../utils/responseBody");
+const ObjectId = require("mongoose").Types.ObjectId;
 
-const validateCreateShowRequest = (req, res, next) => {
-         const { movieId, theaterId, showtime, screenNumber, availableSeats, pricePerSeat } = req.body;
-         if (!movieId || !theaterId || !showtime || !availableSeats || !pricePerSeat) {
-             return res.status(STATUS.BAD_REQUEST).json({ message: "Missing required fields" });
-         }
-         // Additional validations can be added here (e.g., data types, formats)
-         next();
+const validateCreateShowRequest = async (req, res, next) => {
+  // validate theatre id
+  if (!req.body.theatreId) {
+    errorResponseBody.err = "No theatre provided";
+    return res.status(STATUS.BAD_REQUEST).json(errorResponseBody);
+  }
+  if (!ObjectId.isValid(req.body.theatreId)) {
+    errorResponseBody.err = "Invalid theatre id";
+    return res.status(STATUS.BAD_REQUEST).json(errorResponseBody);
+  }
+  // validate movie presence
+  if (!req.body.movieId) {
+    errorResponseBody.err = "No movie provided";
+    return res.status(STATUS.BAD_REQUEST).json(errorResponseBody);
+  }
+  if (!ObjectId.isValid(req.body.movieId)) {
+    errorResponseBody.err = "Invalid movie id";
+    return res.status(STATUS.BAD_REQUEST).json(errorResponseBody);
+  }
+  // validate timing presence
+  if (!req.body.timing) {
+    errorResponseBody.err = "No timing provided";
+    return res.status(STATUS.BAD_REQUEST).json(errorResponseBody);
+  }
+  // validate noofseats presence
+  if (!req.body.noOfSeats) {
+    errorResponseBody.err = "No seat info provided";
+    return res.status(STATUS.BAD_REQUEST).json(errorResponseBody);
+  }
+  // validate price presence
+  if (!req.body.price) {
+    errorResponseBody.err = "No price information provided";
+    return res.status(STATUS.BAD_REQUEST).json(errorResponseBody);
+  }
+  next();
 };
 
 module.exports = {
-         validateCreateShowRequest,
+  validateCreateShowRequest,
 };
-
