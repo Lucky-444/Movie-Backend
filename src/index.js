02dcd6c8de7593983fb32e5fc19fc2cfed7b2426
Lucky <sashankswain6@gmail.com>
2026-01-10 02:29:25 +0530

    Feat: add CORS support to enable cross-origin requests

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
 dotenv.config();
 
@@ -22,6 +23,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3001", // your frontend origin
+    credentials: true, // allow cookies to be sent
+  })
+);
+
 
 connectDB();
 
